ed71bb4c8ff34041be0650493e375e786b3de123
ampm <>
2023-02-06 16:16:26 +0000

    ch5: in all forms take advantage of form_open function from form_helpers to simplify the adition of csrf and others hidden fields.

--- proj/app/Views/news/create.php
+++ proj/app/Views/news/create.php
@@ -5,20 +5,19 @@
 <?= session()->getFlashdata('error') ?>
 <?= validation_list_errors() ?>
 
-<form action="/news/create" method="post">
-    <?= csrf_field() ?>
-    <p>
-        <label for="title">Title</label>
-        <input id="title" name="title" value="<?= set_value('title') ?>">
-    </p>
+<?= form_open('news/create') ?>
+<p>
+    <label for="title">Title</label>
+    <input id="title" name="title" value="<?= set_value('title') ?>">
+</p>
 
-    <p>
-        <label for="body">Text</label>
-        <textarea id="body" name="body" cols="45" rows="4"><?= set_value('body') ?></textarea>
-    </p>
+<p>
+    <label for="body">Text</label>
+    <textarea id="body" name="body" cols="45" rows="4"><?= set_value('body') ?></textarea>
+</p>
 
-    <button>
-        Create news item
-    </button>
-</form>
+<button>
+    Create news item
+</button>
+<?= form_close() ?>
 <?= $this->endSection() ?>
--- proj/app/Views/news/show.php
+++ proj/app/Views/news/show.php
@@ -8,12 +8,13 @@
     <a href="/news/<?= esc($news['slug'], 'url') ?>/edit">
         Edit article
     </a>
-
-    <form action="/news/delete" method="post">
-        <?= csrf_field() ?>
-        <?= form_hidden('news_id', esc($news['id'])) ?>
-        <button>Delete</button>
-    </form>
+    <?= form_open(
+        'news/delete',
+        [],
+        ['news_id' => esc($news['id'])]
+    ) ?>
+    <button>Delete</button>
+    <?= form_close() ?>
 </section>
 
 <?= $this->endSection() ?>
