2459e50e2608ccd6c2292efc31228ac0679e623e
ampm <>
2023-02-06 18:30:45 +0000

    ch5: update footer with ci version dynamic and copywrite.

--- proj/app/Views/templates/footer.php
+++ proj/app/Views/templates/footer.php
@@ -3,14 +3,15 @@
 
         <p>Page rendered in {elapsed_time} seconds</p>
 
-        <p>Environment: <?= ENVIRONMENT ?></p>
-
+        <p>
+            Environment: <?= ENVIRONMENT ?> - CI version: <?= \CodeIgniter\CodeIgniter::CI_VERSION ?>
+        </p>
     </div>
 
     <div class="copyrights">
 
-        <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
-            open source licence.</p>
+        <p>&copy; <?= date('Y') ?> AMPM
+        </p>
 
     </div>
 
