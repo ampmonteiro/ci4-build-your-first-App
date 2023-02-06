b251eb9a3a099e87cccca401abc271e8ed648587
ampm <>
2023-02-06 18:20:52 +0000

    ch5: better look in create and edit forms.

--- proj/app/Views/news/create.php
+++ proj/app/Views/news/create.php
@@ -6,12 +6,12 @@
 <?= validation_list_errors() ?>
 
 <?= form_open('news/store') ?>
-<p>
+<p class="form-control">
     <label for="title">Title</label>
     <input id="title" name="title" value="<?= old('title') ?>">
 </p>
 
-<p>
+<p class="form-control">
     <label for="body">Text</label>
     <textarea id="body" name="body" cols="45" rows="4"><?= old('body') ?></textarea>
 </p>
--- proj/app/Views/news/edit.php
+++ proj/app/Views/news/edit.php
@@ -9,12 +9,12 @@
     'news/update',
     hidden: ['news_id' => esc($news['id'])]  # take advantage of named args since php 8
 ) ?>
-<p>
+<p class="form-control">
     <label for="title">Title</label>
     <input id="title" name="title" value="<?= old('title') ?? $news['title'] ?>">
 </p>
 
-<p>
+<p class="form-control">
     <label for="body">Text</label>
     <textarea id="body" name="body" cols="45" rows="4"><?= old('body') ?? $news['title'] ?></textarea>
 </p>
